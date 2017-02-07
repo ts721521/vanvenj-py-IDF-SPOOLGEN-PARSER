ee2f678046f2fa92d91783e17d766b83e41fe844
vanvenj <vanvenj@gmail.com>
2017-02-07 14:03:44 +0800

    add set mto control on method

--- pyIdf.py
+++ pyIdf.py
@@ -184,7 +184,28 @@ def mergeMaterialList(materialList,materialCodeList):
     return materialList
 
 
+def setMtoCOn(filename):
+    """
+    PDMS中控制材料是否显示、是否标注使用MTOC控制
+    在IDF中以数字的形式标识
+    关系如下 ON:0,OFF:1000000,DATU:1100000,DATD:12000000
+    此函数通过查找这个值，并且替换成0
+
+    """
+    #打开文件，读取整个文件
+    f = open(filename,'r')
+    s = f.read()
+    f.close()
     
+    #利用正则表达式替换
+    s = re.sub(r"(?<=,\s)1[012]00000(?=\s,)","      0",s)  
+
+    #保存文档
+    f = open(filename,'w')
+    f.write(s)
+    f.close()
+
+
 
 if __name__ == '__main__':
     #创建带标题的空文件
