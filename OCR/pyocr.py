3f77cdb116a246253cf1fd5065de2d9120089b62
vanvenj <vanvenj@gmail.com>
2017-02-21 16:23:15 +0800

    增加测试代码

--- OCR/pyocr.py
+++ OCR/pyocr.py
@@ -20,9 +20,19 @@ def get_data_with_re(json_file):
     with open(json_file,'r') as f:
         lines = re.findall(r'{"WordText".+?}',f.read())
     return lines
-    
+
+#输出频率分布柱状图
+def plotHistogram(x,maxX):
+    plt.hist(x,100)
+    plt.xlabel('Position')
+    plt.xlim(0.0,maxX)
+    plt.ylabel('Frequency')
+    plt.title('Pos Frequency')
+    plt.show()
+
 
 if __name__ == '__main__':
+    ##obtain json file form api
     #test_file = ocr_space_file(filename='sample.png')
     #with open('sample.json','w') as f:
     #    f.write(test_file)
@@ -30,12 +40,39 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from PIL import Image
     img = Image.open('sample.png')
+
+    #parse json data  
     words = get_data_with_re('sample.json')
-    plt.figure('ocr')
-    plt.imshow(img)
 
-    for word in words:
-        w = json.loads(word)
-        plt.text(w['Left'],w['Top'] + w['Height'],w['WordText'],fontsize=w['Height'],color='red')
+    print map((lambda w : [w['Left'],w['Top'] + w['Height'],w['WordText'],w['Height'],w['Width'], (w['Top'] + w['Height']) / 15 ]),words)
+    
+### 获取Y坐标    
+##    wordDict = []
+##    for word in words:
+##        w = json.loads(word)
+##        wordDict.append([w['Left'],w['Top'] + w['Height'],w['WordText'],w['Height'],w['Width'], (w['Top'] + w['Height']) / 15 ])
+##
 
-    plt.show()
+# 绘制柱状图
+##    xPos = []
+##    for _ in wordDict:
+##        xPos.append(_[0])
+##
+##    plotHistogram(xPos,700)
+
+
+    
+# 按矫正Y坐标排序
+##    for i in sorted(wordDict,key = lambda x: x[5]):
+##        print i
+    
+
+# 在图片中显示识别信息
+##    plt.figure('ocr')
+##    plt.imshow(img)
+##    
+##    for word in words:
+##        w = json.loads(word)
+##        plt.text(w['Left'],w['Top'] + w['Height'],w['WordText'],fontsize=w['Height'],color='red')
+##
+##    plt.show()
