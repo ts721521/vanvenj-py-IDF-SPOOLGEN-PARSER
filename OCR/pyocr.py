f897b64429de15101d010aeee5efd5269a20ec6e
Fan Fan <fan@Fans-Mac-mini.local>
2017-02-21 22:06:29 +0800

    fix plt histogram method

--- OCR/pyocr.py
+++ OCR/pyocr.py
@@ -14,51 +14,67 @@ def ocr_space_file(filename, overlay=True, api_key='helloworld', language='eng')
                           )
     return r.content.decode()
 
-#使用正则表达式提取返回文件中的字符信息
 def get_data_with_re(json_file):
+    '''
+    使用正则表达式提取返回文件中的字符信息
+    返回格式如下：
+    [
+        [Left,Top,WordText,Height,Width],
+        [Left,Top,WordText,Height,Width],
+        ...
+    ]
+    '''
     import re
+    import json
+    words = []
     with open(json_file,'r') as f:
         lines = re.findall(r'{"WordText".+?}',f.read())
-    return lines
+    for line in lines:
+        word = json.loads(line)
+        words.append([word['Left'],word['Top'],word['WordText'],word['Height'],word['Width']])
+    return words
 
-#输出频率分布柱状图
-def plotHistogram(x,maxX):
-    plt.hist(x,100)
-    plt.xlabel('Position')
-    plt.xlim(0.0,maxX)
+def plotHistogram(x):
+    '''
+    输出频率分布柱状图
+    '''
+    import numpy as np
+    bins = np.arange(min(x),max(x),1)
+    data =  plt.hist(x,bins=bins)
+    plt.xlabel('Value')
+    plt.xlim(min(x)-10,max(x)+10)
     plt.ylabel('Frequency')
-    plt.title('Pos Frequency')
+    plt.title('Frequency Count')
     plt.show()
-
+    return data
 
 if __name__ == '__main__':
     ##obtain json file form api
     #test_file = ocr_space_file(filename='sample.png')
     #with open('sample.json','w') as f:
     #    f.write(test_file)
+
     import json 
     import matplotlib.pyplot as plt
     from PIL import Image
     img = Image.open('sample.png')
 
-    #parse json data  
+    #获取解析后的json data  
     words = get_data_with_re('sample.json')
 
-    print map((lambda w : [w['Left'],w['Top'] + w['Height'],w['WordText'],w['Height'],w['Width'], (w['Top'] + w['Height']) / 15 ]),words)
-    
-### 获取Y坐标    
-##    wordDict = []
-##    for word in words:
-##        w = json.loads(word)
-##        wordDict.append([w['Left'],w['Top'] + w['Height'],w['WordText'],w['Height'],w['Width'], (w['Top'] + w['Height']) / 15 ])
-##
+    #获取x，y，Height的数据集
+    xPositions = [(lambda x:x[0])(x) for x in words]
+    yPositions = [(lambda x:x[1])(x) for x in words]
+    Heights = [(lambda x:x[3])(x) for x in words]
 
-# 绘制柱状图
-##    xPos = []
-##    for _ in wordDict:
-##        xPos.append(_[0])
-##
-##    plotHistogram(xPos,700)
+    import collections
+    c = collections.Counter(yPositions)
+    yPosCount = zip(c.keys(),c.values())
+    print sorted(yPosCount,key = lambda x:x[0])
+
+    #绘制柱状图
+    plotHistogram(yPositions)
+
 
 
     
@@ -67,12 +83,10 @@ if __name__ == '__main__':
 ##        print i
     
 
-# 在图片中显示识别信息
-##    plt.figure('ocr')
-##    plt.imshow(img)
-##    
-##    for word in words:
-##        w = json.loads(word)
-##        plt.text(w['Left'],w['Top'] + w['Height'],w['WordText'],fontsize=w['Height'],color='red')
-##
-##    plt.show()
+    #在图片中显示识别信息
+    #plt.figure('ocr')
+    #plt.imshow(img)
+    #
+    #for w in words:
+    #    plt.text(w[0],round((w[1] + w[3]) / 10.0 )* 10 ,w[2],fontsize=10,color='red')
+    #plt.show()
